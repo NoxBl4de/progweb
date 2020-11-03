069516926b6082b404fbf3306f0403ed225b0474
HDEROCHE <hugo.deroche@gmail.com>
2020-11-03 15:47:52 +0100

    safe commit

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <md-toolbar class="md-primary">
+        <h3 class="md-title">Primary</h3>
+    </md-toolbar>
+</template>
+
+<script>
+import Vue from 'vue'
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
+
+export default {
+
+}
+</script>
+
+<stylelang="scss" scoped>
+  .md-toolbar + .md-toolbar {
+    margin-top: 16px;
+  }
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -3,5 +3,8 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
 
 createApp(App).use(store).use(router).mount('#app')
+App.use(VueMaterial)
